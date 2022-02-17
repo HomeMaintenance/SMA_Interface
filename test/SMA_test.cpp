2bb863e994f022b7208409ec2e10e87be070b26a
Planet-Moon <topwastaken@abwesend.de>
2022-02-17 22:37:57 +0100

    add output

--- test/SMA_test.cpp
+++ test/SMA_test.cpp
@@ -20,22 +20,36 @@ private:
 void storageBoy_test(){
     SMA::StorageBoy myStorageBoy("192.168.178.113");
     auto soc = myStorageBoy.soc();
+    std::cout << "soc: " << soc << std::endl;
     auto present_discharge = myStorageBoy.present_discharge();
+    std::cout << "present_discharge: " << present_discharge << std::endl;
     auto present_charge = myStorageBoy.present_charge();
+    std::cout << "present_charge: " << present_charge << std::endl;
     auto charged_energy = myStorageBoy.charged_energy();
+    std::cout << "charged_energy: " << charged_energy << std::endl;
     auto max_capacity = myStorageBoy.max_capacity();
+    std::cout << "max_capacity: " << max_capacity << std::endl;
     auto missing_charge = myStorageBoy.missing_charge();
+    std::cout << "missing_charge: " << missing_charge << std::endl;
     auto max_charge_rate = myStorageBoy.max_charge_rate();
+    std::cout << "max_charge_rate: " << max_charge_rate << std::endl;
     auto max_discharge_rate = myStorageBoy.max_discharge_rate();
+    std::cout << "max_discharge_rate: " << max_discharge_rate << std::endl;
+    std::cout << "-----------------" << std::endl;
     return;
 }
 
 void device_test(){
     SMA::Device myDevice("192.168.178.128");
     auto dcWatt = myDevice.get_dcWatt();
+    std::cout << "dcWatt: " << dcWatt << std::endl;
     auto mainsFeedIn = myDevice.get_mainsFeedIn();
+    std::cout << "mainsFeedIn: " << mainsFeedIn << std::endl;
     auto mainsSupply = myDevice.get_mainsSupply();
+    std::cout << "mainsSupply: " << mainsSupply << std::endl;
     auto power = myDevice.get_power();
+    std::cout << "power: " << power << std::endl;
+    std::cout << "-----------------" << std::endl;
     return;
 }
 
